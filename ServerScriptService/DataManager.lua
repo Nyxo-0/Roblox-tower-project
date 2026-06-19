dc6e3ddc7c1ce826cba345c3ad6b101c471e4588
Nyxo <ninjatycoonreal@gmail.com>
2026-06-19 17:40:59 -0400

    Delete ServerScriptService directory

--- ServerScriptService/DataManager.lua
+++
@@ -1,7 +0,0 @@
--- @ScriptType: Script
-local SS = game:GetService("ServerStorage")
-local Players = game:GetService("Players")
-
-local dataHandler = require(SS:WaitForChild("DataHandler"))
-
-dataHandler:Init()
