8ab512c992ede6000500bc3ad1f2f374fc5b1bde
Nyxo <ninjatycoonreal@gmail.com>
2026-06-19 17:38:25 -0400

    Updated ServerStorage/DataHandler/DataTemplate.lua

---
+++ ServerStorage/DataHandler/DataTemplate.lua
@@ -0,0 +1,19 @@
+-- @ScriptType: ModuleScript
+local template = {
+	["leaderstats"] = {
+		["Cash"] = 0;
+		["Time"] = 0;
+	};
+	["Misc"] = {
+		["Age"] = 0;
+		["rand"] = "test";
+		["amtoeh"] = {
+			["etsttt"] = {
+				["vassd"] = "wda";
+				["b"] = true;
+			};
+		};
+	};
+}
+
+return template
